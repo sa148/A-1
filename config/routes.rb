e2ed1975f813a8971e0490e1184ab8fb1c0372e1
sa148 <s.kurono@outlook.jp>
2022-09-19 14:14:44 +0000

    adddestroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'lists/new'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+ get 'top' => 'homes#top'
+  resources :lists
 end
